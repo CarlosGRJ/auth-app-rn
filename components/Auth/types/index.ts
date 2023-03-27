20f7b7e62f878c4edc690fff0b3f4e3c685b30d7
CarlosGRJ <carlosgrj@outlook.com>
2023-03-26 20:20:42 -0600

    first commit

---
+++ components/Auth/types/index.ts
@@ -0,0 +1,13 @@
+export interface ICredentials {
+  email: string;
+  confirmEmail: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface ICredentialsValidation {
+  email: boolean;
+  confirmEmail: boolean;
+  password: boolean;
+  confirmPassword: boolean;
+}
